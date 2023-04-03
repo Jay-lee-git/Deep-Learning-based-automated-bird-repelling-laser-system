315cdf78f0978074cd24de958a4a16f71e08f6a8
ane3 <jeheelee1@gmail.com>
2023-04-03 16:45:18 +0900

    teleop

--- src/read_write.py
+++ src/read_write.py
@@ -1,5 +1,7 @@
 import os
 from dynamixel_sdk import * # Uses Dynamixel SDK library
+import cv2
+import curses
 
 def pos_to_angle(pos):
     return int(pos/4095*365)
@@ -38,6 +40,7 @@ def enable_torque(packetHandler, portHandler_list, DXL_ID):
         print("Dynamixel has been successfully connected")
 
 
+
 if __name__ == '__main__':
     if os.name == 'nt':
         import msvcrt
@@ -64,7 +67,7 @@ if __name__ == '__main__':
     BAUDRATE                    = 1000000
     PROTOCOL_VERSION            = 2.0
 
-    DXL_ID_list = [11, 12, 13]
+    DXL_ID_list = [11, 12, 13, 14]
     DEVICE_NUM = len(DXL_ID_list)
     DEVICENAME = '/dev/ttyUSB1'
 
@@ -75,11 +78,7 @@ if __name__ == '__main__':
 
     index = 0
     # Goal position
-    dxl_goal_position = [
-                        [angle_to_pos(180), angle_to_pos(180)],
-                        [angle_to_pos(180 - 20), angle_to_pos(180+20)],
-                        [angle_to_pos(180), angle_to_pos(180)]]
-
+    dxl_goal_position = [angle_to_pos(180), angle_to_pos(100), angle_to_pos(250), angle_to_pos(180)]
 
     portHandler_list = [PortHandler(DEVICENAME) for _ in range(DEVICE_NUM)]
     packetHandler_list = [PacketHandler(PROTOCOL_VERSION) for _ in range(DEVICE_NUM)]
@@ -90,13 +89,22 @@ if __name__ == '__main__':
 
     while True:
         # print("Press any key to continue! (or press ESC to quit!)")
-        if getch() == chr(0x1b):
+        key_input = getch()
+        if key_input == chr(0x1b):
             break
+        elif key_input == 'w':
+            dxl_goal_position[3] -= angle_to_pos(1)
+        elif key_input == 's':
+            dxl_goal_position[3] += angle_to_pos(1)
+        elif key_input == 'a':
+            dxl_goal_position[0] += angle_to_pos(1)
+        elif key_input == 'd':
+            dxl_goal_position[0] -= angle_to_pos(1)
 
         # Write goal position
         for i in range(0, DEVICE_NUM):
             dxl_comm_result, dxl_error = packetHandler_list[i].write4ByteTxRx(portHandler_list[i], 
-                                                                    DXL_ID_list[i], ADDR_GOAL_POSITION, dxl_goal_position[i][index])
+                                                                    DXL_ID_list[i], ADDR_GOAL_POSITION, dxl_goal_position[i])
         
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler_list[0].getTxRxResult(dxl_comm_result))
@@ -114,12 +122,10 @@ if __name__ == '__main__':
                 elif dxl_error != 0:
                     print("%s" % packetHandler_list[i].getRxPacketError(dxl_error))
 
-            # for i in range(0, DEVICE_NUM):
-
-                print(f'id:{DXL_ID_list[i]}, GoalPos :{pos_to_angle(dxl_goal_position[i][index])}({dxl_goal_position[i][index]}) | PresPos:{pos_to_angle(dxl_present_position)}({dxl_present_position})')
+                print(f'id:{DXL_ID_list[i]}, GoalPos :{pos_to_angle(dxl_goal_position[i])}({dxl_goal_position[i]}) | PresPos:{pos_to_angle(dxl_present_position)}({dxl_present_position})')
 
 
-                if not abs(dxl_goal_position[i][index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+                if not abs(dxl_goal_position[i] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
                     break_bool = True
                     break
             print('=====================================')
@@ -128,8 +134,6 @@ if __name__ == '__main__':
 
         if index == 0:
             index = 1
-        else:
-            index = 0
 
 
     # Disable Dynamixel Torque
